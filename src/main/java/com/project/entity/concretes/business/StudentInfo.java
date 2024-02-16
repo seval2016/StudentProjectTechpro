75801d3f76130ba9c16cf8f73788c98e0602e5e6
seval <sevalsenturk2016@gmail.com>
2024-02-16 20:25:44 +0300

    first commit

---
+++ src/main/java/com/project/entity/concretes/business/StudentInfo.java
@@ -0,0 +1,52 @@
+package com.project.entity.concretes.business;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.project.entity.concretes.user.User;
+import com.project.entity.enums.Note;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class StudentInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer absentee; // yoklama
+
+    private Double midtermExam;
+
+    private Double finalExam;
+
+    private Double examAverage;
+
+    private String infoNote;
+
+    @Enumerated(EnumType.STRING)
+    private Note letterGrade; // 100 --> AA ,30 --> FF
+
+    @ManyToOne //1 öğretmen 1 den fazla student info ya atanabilir
+    @JsonIgnore
+    private User teacher;
+
+    @ManyToOne // 1 öğrenci aldığı ders sayısı kadar student infosu vardır
+    @JsonIgnore
+    private User student;
+
+    @ManyToOne
+    private Lesson lesson;
+
+    @OneToOne
+    private EducationTerm educationTerm;
+
+}
