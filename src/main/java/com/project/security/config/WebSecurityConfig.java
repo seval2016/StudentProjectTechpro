d3a0bb7d1f90741ad1a69c07d20866d87ec42c21
seval <sevalsenturk2016@gmail.com>
2024-02-17 21:36:53 +0300

    security v4

--- src/main/java/com/project/security/config/WebSecurityConfig.java
+++ src/main/java/com/project/security/config/WebSecurityConfig.java
@@ -1,14 +1,53 @@
 package com.project.security.config;
 
-
+import com.project.security.jwt.AuthEntryPointJwt;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return  configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.cors().and()
+                .csrf().disable()
+                //kimlik doğrulama hatası durumunda nasıl davranılacağını yapılandırır.
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests().antMatchers(AUTH_WHITE_LIST).permitAll()
+                .anyRequest().authenticated();
+
+        http.headers().frameOptions().sameOrigin();
+
+
+    }
+
+    private static final String[] AUTH_WHITE_LIST ={
+            "/",
+            "index.html",
+            "/images/**",
+            "/css/**",
+            "/js/**",
+            "contactMessages/save",
+            "/auth/login"
+    };
 }
